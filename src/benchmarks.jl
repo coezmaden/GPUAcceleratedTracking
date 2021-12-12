fbb9ead4a6f3a34e56deda4ce4ddf0e270dc0b23
Can Ozmaden <git@canozmaden.com>
2021-12-12 22:10:07 +0100

    Fix typo, add algorithm parameter

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -79,66 +79,9 @@ function _run_kernel_wrapper_benchmark(
     )
 end
 
-function _run_kernel_nowrapper_benchmark(
-    gnss,
-    enable_gpu::Val{true}, 
-    num_samples::Int,
-    num_ants::Int,
-    num_correlators::Int,
-)
-    system = gnss(use_gpu = enable_gpu)
-    correlator = EarlyPromptLateCorrelator(NumAnts(num_ants), num_correlators)
-    state = TrackingState(1, system, 1500Hz, 0, num_samples=num_samples, num_ants=NumAnts(num_ants), correlator=correlator)
-    signal, sampling_frequency = gen_signal(system, 1, 1500Hz, num_samples, num_ants=NumAnts(num_ants))
-    correlator_sample_shifts = get_correlator_sample_shifts(system, correlator, sampling_frequency, 0)
-    block_dim_z = num_correlators
-    block_dim_y = num_ants
-    # keep num_corrs and num_ants in seperate dimensions, truncate num_samples accordingly to fit
-    block_dim_x = prevpow(2, 1024 ÷ block_dim_y ÷ block_dim_z)
-    threads = (block_dim_x, block_dim_y, block_dim_z)
-    blocks = cld(size(signal, 1), block_dim_x)
-    res_re = CUDA.zeros(Float32, blocks, block_dim_y, block_dim_z)
-    res_im = CUDA.zeros(Float32, blocks, block_dim_y, block_dim_z)
-    shmem_size = sizeof(ComplexF32)*block_dim_x*block_dim_y*block_dim_z
-    @benchmark CUDA.@sync @cuda threads=threads blocks=blocks shmem=shmem_size Tracking.downconvert_and_correlate_kernel(
-        $res_re, 
-        $res_im, 
-        $signal.re, 
-        $signal.im,
-        $system.codes,
-        $Float32(get_code_frequency(system)),
-        $correlator_sample_shifts,
-        $Float32(1500Hz),
-        $Float32(sampling_frequency),
-        $Float32(0),
-        $Float32(0.0),
-        $get_code_length(system),
-        1,
-        $num_samples, 
-        $num_ants,
-        $num_correlators
-    )
-end
-
-function _run_kernel_nowrapper_benchmark(
-    gnss,
-    enable_gpu::Val{false}, 
-    num_samples::Int,
-    num_ants::Int,
-    num_correlators::Int,
-)
-    _run_kernel_wrapper_benchmark(
-        gnss,
-        enable_gpu::Val{false}, 
-        num_samples::Int,
-        num_ants::Int,
-        num_correlators::Int,
-    )
-end
-
 function do_track_benchmark(benchmark_params::Dict)
-    @unpack GNSS, num_samples, num_ants, num_correlators, processor, OS = benchmark_params
-    @debug "[$(Dates.Time(Dates.now()))] Benchmarking: $(GNSS), $(num_samples) samples,  $(num_ants) antenna,  $(num_correlators) correlators $(processor)"
+    @unpack GNSS, num_samples, num_ants, num_correlators, processor, OS, algorithm = benchmark_params
+    @debug "[$(Dates.Time(Dates.now()))] Benchmarking: $(GNSS), $(num_samples) samples,  $(num_ants) antenna,  $(num_correlators) correlators $(processor) w/ Algorithm:$(algorithm)"
     enable_gpu = (processor == "GPU" ? Val(true) : Val(false))
     cpu_name = Sys.cpu_info()[1].model
     cpu_name == "unkown" ? "NVIDIA ARMv8" : cpu_name
@@ -163,11 +106,11 @@ function do_track_benchmark(benchmark_params::Dict)
 end
 
 function do_kernel_benchmark(benchmark_params::Dict)
-    @unpack GNSS, num_samples, num_ants, num_correlators, processor, OS = benchmark_params
-    @debug "[$(Dates.Time(Dates.now()))] Benchmarking: $(GNSS), $(num_samples) samples,  $(num_ants) antenna,  $(num_correlators) correlators $(processor)"
+    @unpack GNSS, num_samples, num_ants, num_correlators, processor, OS, algorithm = benchmark_params
+    @debug "[$(Dates.Time(Dates.now()))] Benchmarking: $(GNSS), $(num_samples) samples,  $(num_ants) antenna,  $(num_correlators) correlators $(processor) w/ Algorithm:$(algorithm)"
     enable_gpu = (processor == "GPU" ? Val(true) : Val(false))
     cpu_name = Sys.cpu_info()[1].model
-    cpu_name == "unkown" ? "NVIDIA ARMv8" : cpu_name
+    cpu_name == "unknown" ? "NVIDIA ARMv8" : cpu_name
     processor_name = processor == "GPU" ? name(CUDA.CuDevice(0)) : cpu_name
     benchmark_results = _run_kernel_benchmark(
         GNSSDICT[GNSS], 
