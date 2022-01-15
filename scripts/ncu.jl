b9bca453edc7a77c43c55e896414b0081b9d26e4
Can Ozmaden <git@canozmaden.com>
2022-01-15 16:04:40 +0100

    Clean up dependencies, add nvprof script

--- scripts/ncu.jl
+++ scripts/ncu.jl
@@ -1,11 +1,12 @@
 # nvprof --profile-from-start off julia /path/to/ncu.jl
 # or
 # ncu --mode=launch julia /path/to/ncu.jl
-
+using CUDA
+CUDA.version()
 using GPUAcceleratedTracking, CUDA, Tracking, GNSSSignals, StructArrays
 import Tracking: Hz, ms
 
-system = GPSL1(use_gpu = Val(true))
+system = GPSL1(use_gpu = Val(true));
 
 ## 1 ms signal track
 # signal, sampling_frequency = gen_signal(system, 1, 1500Hz, 2500, num_ants = NumAnts(4))
@@ -15,61 +16,48 @@ system = GPSL1(use_gpu = Val(true))
 
 ## 1 ms signal downconvert and correlate
 # Generate GNSS object and signal information
-enable_gpu = Val(true)
-num_samples = 50000
-num_ants = 1
-num_correlators = 3
-algorithm = KernelAlgorithm(5)
+enable_gpu = Val(true);
+num_samples = 50000;
+num_ants = 1;
+num_correlators = 3;
+algorithm = KernelAlgorithm(5);
 
-system = GPSL1(use_gpu = enable_gpu)
-codes = CuTexture(CuTextureArray(system.codes))
-code_frequency = get_code_frequency(system)
-code_length = get_code_length(system)
-start_code_phase = 0.0f0
-carrier_phase = 0.0f0
-carrier_frequency = 1500Hz
-prn = 1
+system = GPSL1(use_gpu = enable_gpu);
+codes = CuTexture(CuTextureArray(system.codes));
+code_frequency = get_code_frequency(system);
+code_length = get_code_length(system);
+start_code_phase = 0.0f0;
+carrier_phase = 0.0f0;
+carrier_frequency = 1500Hz;
+prn = 1;
 
-# Generate the signal
-signal, sampling_frequency = gen_signal(system, prn, carrier_frequency, num_samples, num_ants = NumAnts(num_ants), start_code_phase = start_code_phase, start_carrier_phase = carrier_phase)
+# Generate the signal;
+signal, sampling_frequency = gen_signal(system, prn, carrier_frequency, num_samples, num_ants = NumAnts(num_ants), start_code_phase = start_code_phase, start_carrier_phase = carrier_phase);
 
-# Generate correlator
-correlator = EarlyPromptLateCorrelator(NumAnts(num_ants), NumAccumulators(num_correlators))
-correlator_sample_shifts = get_correlator_sample_shifts(system, correlator, sampling_frequency, 0.5)
-num_of_shifts = correlator_sample_shifts[end] - correlator_sample_shifts[1]
+# Generate correlator;
+correlator = EarlyPromptLateCorrelator(NumAnts(num_ants), NumAccumulators(num_correlators));
+correlator_sample_shifts = get_correlator_sample_shifts(system, correlator, sampling_frequency, 0.5);
+num_of_shifts = correlator_sample_shifts[end] - correlator_sample_shifts[1];
 
-# Generate blank code and carrier replica, and downconverted signal
-code_replica = CUDA.zeros(Float32, num_samples + num_of_shifts)
-carrier_replica = StructArray{ComplexF32}((CUDA.zeros(Float32, num_samples), CUDA.zeros(Float32, num_samples)))
-downconverted_signal = StructArray{ComplexF32}((CUDA.zeros(Float32, num_samples, num_ants), CUDA.zeros(Float32, num_samples, num_ants)))
+# Generate blank code and carrier replica, and downconverted signal;
+code_replica = CUDA.zeros(Float32, num_samples + num_of_shifts);
+carrier_replica = StructArray{ComplexF32}((CUDA.zeros(Float32, num_samples), CUDA.zeros(Float32, num_samples)));
+downconverted_signal = StructArray{ComplexF32}((CUDA.zeros(Float32, num_samples, num_ants), CUDA.zeros(Float32, num_samples, num_ants)));
 
-# Generate CUDA kernel tuning parameters
-threads_per_block = [1024, 512÷2]
-blocks_per_grid = cld.(num_samples, threads_per_block)
-partial_sum = StructArray{ComplexF32}((CUDA.zeros(Float32, (blocks_per_grid[2], num_ants, length(correlator_sample_shifts))),CUDA.zeros(Float32, (blocks_per_grid[2], num_ants, length(correlator_sample_shifts)))))
-shmem_size = sizeof(ComplexF32) * threads_per_block[2] * num_correlators * num_ants
+# Generate CUDA kernel tuning parameters;
+threads_per_block = [1024, 512÷2];
+blocks_per_grid = cld.(num_samples, threads_per_block);
+partial_sum = StructArray{ComplexF32}((CUDA.zeros(Float32, (blocks_per_grid[2], num_ants, length(correlator_sample_shifts))),CUDA.zeros(Float32, (blocks_per_grid[2], num_ants, length(correlator_sample_shifts)))));
+shmem_size = sizeof(ComplexF32) * threads_per_block[2] * num_correlators * num_ants;
 
-@cuda threads=threads_per_block[1] blocks=blocks_per_grid[1] gen_code_replica_kernel!(
-            code_replica,
-            codes,
-            code_frequency,
-            sampling_frequency,
-            start_code_phase,
-            prn,
-            num_samples,
-            num_of_shifts,
-            code_length
-        )
-CUDA.@profile NVTX.@range "kernel_algorithm" begin
-    @cuda threads=threads_per_block[1] blocks=blocks_per_grid[1] gen_code_replica_kernel!(
-            code_replica,
-            codes,
-            code_frequency,
-            sampling_frequency,
-            start_code_phase,
-            prn,
-            num_samples,
-            num_of_shifts,
-            code_length
-        )
-end
+CUDA.@profile @cuda threads=threads_per_block[1] blocks=blocks_per_grid[1] gen_code_replica_kernel!(
+        code_replica,
+        codes,
+        code_frequency,
+        sampling_frequency,
+        start_code_phase,
+        prn,
+        num_samples,
+        num_of_shifts,
+        code_length
+    )
--- scripts/reduce.jl
+++ scripts/reduce.jl
@@ -1,13 +1,65 @@
 using CUDA, Cthulhu, StaticArrays, Tracking, StructArrays, BenchmarkTools
 import Tracking: NumAnts, NumAccumulators
 
+function reduce_block!(
+    gmem_sum,
+    input
+)
+    # define needed incides
+    threads_per_block = blockDim().x  
+    thread_idx  = threadIdx().x
+    idx = thread_idx 
+    
+    # allocate the shared memory for the partial sum
+    shmem_sum = @cuDynamicSharedMem(Float32, threads_per_block)
+    @inbounds shmem_sum[thread_idx] = 0
+
+    # each thread loads one element from global to shared memory
+    # AND
+    # does the first level of reduction
+    @inbounds if idx <= length(input)
+        shmem_sum[thread_idx] = input[idx]
+    end
+
+    # wait until all finished
+    sync_threads() 
+
+    # do reduction in shared memory
+    s::UInt32 = threads_per_block ÷ 2
+    @inbounds while s > 32
+        sync_threads()
+        if thread_idx <= s
+            shmem_sum[thread_idx] += shmem_sum[thread_idx + s]
+        end
+        
+        s ÷= 2
+    end
+    
+    # do warp reduction once tree size = warpsize
+    @inbounds if thread_idx <= 32
+        @inbounds shmem_sum[thread_idx] += shmem_sum[thread_idx - 1 + 32]
+        @inbounds shmem_sum[thread_idx] += shmem_sum[thread_idx - 1 + 16]
+        @inbounds shmem_sum[thread_idx] += shmem_sum[thread_idx - 1 + 8]
+        @inbounds shmem_sum[thread_idx] += shmem_sum[thread_idx - 1 + 4]
+        @inbounds shmem_sum[thread_idx] += shmem_sum[thread_idx - 1 + 2]
+        @inbounds shmem_sum[thread_idx] += shmem_sum[thread_idx - 1 + 1]
+    end
+
+    # first thread returns the result of reduction to global memory
+    @inbounds if thread_idx == 1
+        gmem_sum[1] += shmem_sum[1]
+    end
+
+    return nothing
+end
 
 N = 2^13
 input = CUDA.ones(Float32, N)
 threads_per_block = 1024 
 blocks_per_grid = cld(N, threads_per_block) ÷ 2 # half the grid
-partial_sum = CUDA.zeros(Float32, 4)
-sum = CUDA.zeros(Float32, 1)
+partial_sum = CUDA.zeros(Float32, blocks_per_grid)
+input = CUDA.ones(Float32, length(partial_sum))
 shmem_size = sizeof(Float32) * threads_per_block
-@cuda threads=threads_per_block blocks=blocks_per_grid shmem=shmem_size reduce_block!(partial_sum, input)
-@cuda threads=1                 blocks=1               shmem=shmem_size reduce_block!(sum, partial_sum)
+
+# @cuda threads=threads_per_block blocks=blocks_per_grid shmem=shmem_size reduce_block!(partial_sum, input)
+@cuda threads=1                 blocks=1               shmem=shmem_size reduce_block!(input, input)
