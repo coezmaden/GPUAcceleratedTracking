0d4110f3f7227637bde4ea65f5ed123c3e4003ab
Can Ozmaden <git@canozmaden.com>
2022-01-20 19:20:51 +0100

    Take max shared memory into account

--- test/algorithms.jl
+++ test/algorithms.jl
@@ -223,8 +223,8 @@ end
     block_dim_z = num_correlators
     block_dim_y = num_ants
     # keep num_corrs and num_ants in seperate dimensions, truncate num_samples accordingly to fit
-    block_dim_x = prevpow(2, 512 ÷ block_dim_y ÷ block_dim_z)
-    threads_per_block = [(block_dim_x, block_dim_y, block_dim_z), 1024]
+    block_dim_x = prevpow(2, 256 ÷ block_dim_y ÷ block_dim_z)
+    threads_per_block = [(block_dim_x, block_dim_y, block_dim_z), 512]
     blocks_per_grid = cld(num_samples, block_dim_x) ÷ 2 # launch with half the grid
     partial_sum = StructArray{ComplexF32}((CUDA.zeros(Float32, blocks_per_grid, block_dim_y, block_dim_z), CUDA.zeros(Float32, blocks_per_grid, block_dim_y, block_dim_z)))
     shmem_size = [sizeof(ComplexF32) * block_dim_x * block_dim_y * block_dim_z
@@ -328,10 +328,10 @@ end
     threads_per_block = zeros(Int, 3)
     blocks_per_grid = zeros(Int, 3)
     accum = StructArray{ComplexF32}((CUDA.zeros(Float32, (num_samples, num_ants, length(correlator_sample_shifts))),CUDA.zeros(Float32, (num_samples, num_ants, length(correlator_sample_shifts)))))
-    threads_per_block[3] = 1024
+    threads_per_block[3] = 512
     blocks_per_grid[3] = cld(num_samples, threads_per_block[3])
     phi = StructArray{ComplexF32}((CUDA.zeros(Float32, (blocks_per_grid[3], num_ants, length(correlator_sample_shifts))),CUDA.zeros(Float32, (blocks_per_grid[3], num_ants, length(correlator_sample_shifts)))))
-    shmem_size = sizeof(ComplexF32) * 1024 * num_correlators * num_ants
+    shmem_size = sizeof(ComplexF32) * threads_per_block[3] * num_correlators * num_ants
     code_replica_kernel = @cuda launch=false gen_code_replica_strided_kernel!(
             code_replica,
             codes,
@@ -363,52 +363,52 @@ end
     )
     blocks_per_grid[2], threads_per_block[2] = launch_configuration(downconvert_and_accumulate_kernel.fun)
     algorithm = KernelAlgorithm(2330)
-    # @cuda threads=threads_per_block[1] blocks=blocks_per_grid[1] gen_code_replica_strided_kernel!(
-    #     code_replica,
-    #     codes,
-    #     code_frequency,
-    #     sampling_frequency,
-    #     start_code_phase,
-    #     prn,
-    #     num_samples,
-    #     num_of_shifts,
-    #     code_length
-    # )
-    # @cuda threads=threads_per_block[2] blocks=blocks_per_grid[2] downconvert_and_accumulate_strided_kernel!(
-    #     accum.re,
-    #     accum.im,
-    #     code_replica,
-    #     carrier_replica.re,
-    #     carrier_replica.im,
-    #     downconverted_signal.re,
-    #     downconverted_signal.im,
-    #     signal.re,
-    #     signal.im,
-    #     carrier_frequency,
-    #     sampling_frequency,
-    #     carrier_phase,
-    #     num_samples,
-    #     NumAnts(num_ants),
-    #     correlator_sample_shifts
-    # )
-    # @cuda threads=threads_per_block[3] blocks=blocks_per_grid[3] shmem=shmem_size reduce_cplx_multi_3(
-    #     sum.re,
-    #     sum.im,
-    #     accum.re,
-    #     accum.im,
-    #     num_samples,
-    #     NumAnts(num_ants),
-    #     correlator_sample_shifts
-    # )
-    # @cuda threads=threads_per_block[3] blocks=1 shmem=shmem_size reduce_cplx_multi_3(
-    #     sum.re,
-    #     sum.im,
-    #     sum.re,
-    #     sum.im,
-    #     blocks_per_grid[3],
-    #     NumAnts(num_ants),
-    #     correlator_sample_shifts
-    # )
+    @cuda threads=threads_per_block[1] blocks=blocks_per_grid[1] gen_code_replica_strided_kernel!(
+        code_replica,
+        codes,
+        code_frequency,
+        sampling_frequency,
+        start_code_phase,
+        prn,
+        num_samples,
+        num_of_shifts,
+        code_length
+    )
+    @cuda threads=threads_per_block[2] blocks=blocks_per_grid[2] downconvert_and_accumulate_strided_kernel!(
+        accum.re,
+        accum.im,
+        code_replica,
+        carrier_replica.re,
+        carrier_replica.im,
+        downconverted_signal.re,
+        downconverted_signal.im,
+        signal.re,
+        signal.im,
+        carrier_frequency,
+        sampling_frequency,
+        carrier_phase,
+        num_samples,
+        NumAnts(num_ants),
+        correlator_sample_shifts
+    )
+    @cuda threads=threads_per_block[3] blocks=blocks_per_grid[3] shmem=shmem_size reduce_cplx_multi_3(
+        phi.re,
+        phi.im,
+        accum.re,
+        accum.im,
+        num_samples,
+        NumAnts(num_ants),
+        correlator_sample_shifts
+    )
+    @cuda threads=threads_per_block[3] blocks=1 shmem=shmem_size reduce_cplx_multi_3(
+        phi.re,
+        phi.im,
+        phi.re,
+        phi.im,
+        blocks_per_grid[3],
+        NumAnts(num_ants),
+        correlator_sample_shifts
+    )
     kernel_algorithm(
         threads_per_block,
         blocks_per_grid,
@@ -599,7 +599,7 @@ end
 @testset "Kernel Algorithm 2_4_cplx_multi" begin
     enable_gpu = Val(true)
     num_samples = 2500
-    num_ants = 1
+    num_ants = 4
     num_correlators = 3
     system = GPSL1(use_gpu = enable_gpu)
     codes = system.codes
@@ -622,7 +622,12 @@ end
     threads_per_block[3] = 1024
     blocks_per_grid[3] = cld(num_samples, threads_per_block[3])
     phi = StructArray{ComplexF32}((CUDA.zeros(Float32, (blocks_per_grid[3], num_ants, length(correlator_sample_shifts))),CUDA.zeros(Float32, (blocks_per_grid[3], num_ants, length(correlator_sample_shifts)))))
-    shmem_size = sizeof(ComplexF32) * 1024 * num_correlators * num_ants
+    shmem_size = sizeof(ComplexF32) * threads_per_block[3] * num_correlators * num_ants
+    max_shmem = CUDA.attribute(device(), CUDA.DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK)
+    if max_shmem < shmem_size
+        threads_per_block[3] = max_shmem ÷ (sizeof(ComplexF32) * num_correlators * num_ants)
+        shmem_size = sizeof(ComplexF32) * threads_per_block[3] * num_correlators * num_ants
+    end
     code_replica_kernel = @cuda launch=false gen_code_replica_strided_kernel!(
             code_replica,
             codes,
@@ -887,10 +892,10 @@ end
     end
 end
 
-@testset "Kernel Algorithm 3_4_cplx_multi_textmem" begin
+# @testset "Kernel Algorithm 3_4_cplx_multi_textmem" begin
     enable_gpu = Val(true)
     num_samples = 2500
-    num_ants = 1
+    num_ants = 4
     num_correlators = 3
     system = GPSL1(use_gpu = enable_gpu)
     codes = system.codes
@@ -924,6 +929,15 @@ end
     accum = StructArray{ComplexF32}((CUDA.zeros(Float32, (blocks_per_grid[2], num_ants, length(correlator_sample_shifts))),CUDA.zeros(Float32, (blocks_per_grid[2], num_ants, length(correlator_sample_shifts)))))
     shmem_size[1] = sizeof(ComplexF32) * threads_per_block[2] * num_correlators * num_ants
     shmem_size[2] = sizeof(ComplexF32) *  threads_per_block[3] * num_correlators * num_ants
+    max_shmem = CUDA.attribute(device(), CUDA.DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK)
+    if max_shmem < shmem_size[1]
+        threads_per_block[2] = max_shmem ÷ (sizeof(ComplexF32) * num_correlators * num_ants)
+        shmem_size[2] = sizeof(ComplexF32) * threads_per_block[2] * num_correlators * num_ants
+    end
+    if max_shmem < shmem_size[2]
+        threads_per_block[3] = max_shmem ÷ (sizeof(ComplexF32) * num_correlators * num_ants)
+        shmem_size[2] = sizeof(ComplexF32) * threads_per_block[3] * num_correlators * num_ants
+    end
     code_replica_kernel = @cuda launch=false gen_code_replica_texture_mem_strided_kernel!(
             code_replica,
             codes,
